9b523d02a846b09d1575988a72283e1b27c0fe1f
PMeylemans <patrick@meylemans.net>
2020-03-31 20:06:59 +0000

    debugged on host

--- functions/disable-ipv6.sh
+++ functions/disable-ipv6.sh
@@ -11,20 +11,22 @@
 # Get the colation of this script, used files are relative to this location
 directory="$(dirname "$0")"
 
-found_disable=`grep ipv6.disable /etc/default/grub`
+found_disable=`grep "ipv6.disable" /etc/default/grub | wc -l`
 
-if [ $found_disable .ge 1 ]
+#echo "$found_disable"
+
+if [ "$found_disable" -ge "1" ]
  then
 	clear
 	echo -e "\n\n\n"
 	echo "ipv6 allready disabled on this system"
  else
-    clear
-	echo "GRUB_CMDLINE_LINUX_DEFAULT=\"ipv6.disable=1\" >> /etc/default/grub"
-	echo "GRUB_CMDLINE_LINUX=\"ipv6.disable=1\" >> /etc/default/grub"
+        clear
+	echo "GRUB_CMDLINE_LINUX_DEFAULT=\"ipv6.disable=1\""  >> /etc/default/grub
+	echo "GRUB_CMDLINE_LINUX=\"ipv6.disable=1\"" >> /etc/default/grub
 	update-grub
 	echo -e "\n\n\n"
 	echo "ipv6 disabled, system needs to be rebooted"
 fi
 
-
+#echo "$found_disable"
