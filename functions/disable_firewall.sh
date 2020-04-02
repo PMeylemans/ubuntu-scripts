b6724512fc873aa83960e8591eaf05aa6c45cb41
PMeylemans <52534567+PMeylemans@users.noreply.github.com>
2020-04-02 16:28:19 +0200

    Create disable_firewall.sh

---
+++ functions/disable_firewall.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# Filename : disable_firewall.sh
+#
+# Purpose  : To disable firewall of the system, only use on internal servers
+#
+# History :
+# 2020-04-02  MEY Created.
+# ---------------------------------------------------------------------------
+
+clear
+echo -e "\n\n\n"
+ufw disable
+echo -e "\n\n\n"
+echo "Firewall has been disabled"
